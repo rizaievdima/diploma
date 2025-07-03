6a5d9c5b46a8d4b261333449b92a62b58f759f68
rizaievdima <rizaievdima@gmail.com>
2025-07-03 10:25:10 +0300

    added imports for images v2

--- frontend/src/helpers/additionalFanctions.js
+++ frontend/src/helpers/additionalFanctions.js
@@ -1,7 +1,13 @@
 export const getImageUrl = (name = "") => {
-    const images = import.meta.glob("../assets/images/*.{png,jpg,jpeg,svg}", { eager: true });
+    const images = import.meta.glob("../assets/images/*.{png,jpg,jpeg,svg}", {
+        eager: true,
+        import: "default",
+    });
+    const entry = Object.entries(images).find(([path]) => path.includes(name));
     const imageModule = Object.keys(images).find((path) => path.includes(name));
     if (imageModule) {
+        console.log(new URL(imageModule, import.meta.url).href);
+        console.log(entry);
         return new URL(imageModule, import.meta.url).href;
     }
     return "";
