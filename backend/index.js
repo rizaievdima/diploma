035469833b2244fc5df37866f3b6d87245a8b4c3
rizaievdima <rizaievdima@gmail.com>
2025-07-03 11:33:11 +0300

    changed port settings v1

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-const app = require('./app');
-const PORT = 4000;
+const app = require("./app");
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
